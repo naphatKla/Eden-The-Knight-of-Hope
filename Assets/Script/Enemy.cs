aad82708ac06330332304cdee6317fd63f83d189
Kla <naphat.watt@bumail.net>
2023-09-03 08:08:32 +0700

    Clean a bit code
    - Remove unnecessary while loop in
      Ienumurator.
    - Fixed Roam Logic to make it easier.

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,18 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Serialization;
-using UnityEngine.UIElements;
-using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     #region Declare Variable
+
     public enum EnemyState
     {
         Idle,
@@ -20,13 +15,14 @@ public class Enemy : MonoBehaviour
         ReturnToSpawn,
         WaitToReturn
     }
+
     public enum PriorityTag
     {
         Player,
         NPC,
         Tower
     }
-    
+
     [SerializeField] private float viewDistance;
     [SerializeField] private LayerMask playerLayerMask;
     [SerializeField] private List<PriorityTag> priorityTags;
@@ -39,11 +35,12 @@ public class Enemy : MonoBehaviour
     private Vector2 _spawnPoint;
     private bool _isWait;
     private bool _isRoam;
-    private float _roamCooldownCounter;
     public EnemyState enemyActionState;
+
     #endregion
 
     #region Unity Method
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -54,84 +51,85 @@ public class Enemy : MonoBehaviour
         _spawnPoint = transform.position;
         enemyActionState = EnemyState.Idle;
     }
-    
+
     void Update()
     {
-        Collider2D[] targetInDistances = Physics2D.OverlapCircleAll(transform.position, viewDistance,playerLayerMask);
- 
+        Collider2D[] targetInDistances = Physics2D.OverlapCircleAll(transform.position, viewDistance, playerLayerMask);
+
         if (targetInDistances.Length > 0)
         {
-            
-            // set target to the first priority tag
+            // Detect targets only on the first priority tag found
             for (int i = 0; i < priorityTags.Count; i++)
             {
                 foreach (Collider2D col in targetInDistances)
                 {
                     if (!col.gameObject.CompareTag(priorityTags[i].ToString())) continue;
-                    
+
                     _target = col.gameObject;
-                    i += priorityTags.Count; // break out loop
+                    i += priorityTags.Count; // break out the loop
                     break;
                 }
             }
+
             SetEnemyState(EnemyState.FollowTarget);
         }
         else if (enemyActionState == EnemyState.FollowTarget)
         {
-            
             SetEnemyState(EnemyState.WaitToReturn);
         }
         else if ((Vector2)transform.position == _spawnPoint)
         {
             SetEnemyState(EnemyState.Idle);
         }
-        
+
         PlayAction(enemyActionState);
-        Debug.Log(enemyActionState);
     }
-    
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, viewDistance);
     }
 
-    private IEnumerator SetEnemyState(EnemyState state, float time = 0)
+    private IEnumerator WaitToReturn(float time = 0)
     {
-        float timeCount = 0;
-
-        while (timeCount < time)
-        {
-            timeCount += Time.deltaTime;
-            _isWait = true;
-            yield return null;
-        }
-
+        _isWait = true;
+        yield return new WaitForSeconds(time);
         _isWait = false;
-        enemyActionState = state;
+        enemyActionState = EnemyState.ReturnToSpawn;
     }
 
     private IEnumerator RoamAround(float time)
     {
         float _timeCount = 0;
-        Vector2 randomPos = new Vector2(_spawnPoint.x + Random.Range(roamArea.x, roamArea.y), _spawnPoint.y + Random.Range(roamArea.x, roamArea.y));
-        
+        Vector2 randomPos = new Vector2(_spawnPoint.x + Random.Range(roamArea.x, roamArea.y),
+            _spawnPoint.y + Random.Range(roamArea.x, roamArea.y));
+        _isRoam = true;
+
+        yield return new WaitForSeconds(roamCooldown);
+
         while (_timeCount < time)
         {
             _isRoam = true;
-            _agent.SetDestination(randomPos);
             _timeCount += Time.deltaTime;
+            _agent.SetDestination(randomPos);
+
             if (enemyActionState != EnemyState.Idle)
+            {
+                _isRoam = false;
                 yield break;
+            }
+
             yield return null;
         }
 
-        _roamCooldownCounter = 0;
         _isRoam = false;
     }
+
     #endregion
 
     #region Method
+
     public void SetEnemyState(EnemyState state)
     {
         enemyActionState = state;
@@ -143,8 +141,6 @@ public class Enemy : MonoBehaviour
         {
             case EnemyState.Idle:
             {
-                _roamCooldownCounter += Time.deltaTime;
-                if(_roamCooldownCounter < roamCooldown) return;
                 if (_isRoam) return;
                 StartCoroutine(RoamAround(roamDuration));
                 break;
@@ -162,12 +158,13 @@ public class Enemy : MonoBehaviour
             case EnemyState.WaitToReturn:
             {
                 _agent.SetDestination(_target.transform.position);
-                
-                if(!_isWait)
-                    StartCoroutine(SetEnemyState(EnemyState.ReturnToSpawn, 3));
+
+                if (!_isWait)
+                    StartCoroutine(WaitToReturn(3));
                 break;
             }
         }
     }
+
     #endregion
 }
