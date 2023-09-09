357c649d27cad33293b7bec66c2a15739839090e
Kla <naphat.watt@bumail.net>
2023-09-09 22:21:19 +0700

    Player Animation Guideline Added

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     #region Declare Variable
+
     public enum PlayerState
     {
         Idle,
@@ -13,30 +15,35 @@ public class Player : MonoBehaviour
         Sprint,
         Dash
     }
-    [Header("Player Movement")]
+
+    [Header("Player Movement")] 
     [SerializeField] private float walkSpeed;
     [SerializeField] private float sprintSpeed;
     [SerializeField] private float dashSpeed;
     [SerializeField] private float dashDuration;
     [SerializeField] private float dashDelay;
+    public PlayerState playerState;
     private Rigidbody2D _playerRigidbody2D;
-    public PlayerState _playerState;
+    private Animator _animator;
     private float _currentSpeed;
     private bool _isDash;
+
     #endregion
 
     #region Unity Method
-    void Start()
+
+    private void Start()
     {
+        _animator = GetComponent<Animator>();
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
-        _playerState = PlayerState.Idle;
+        playerState = PlayerState.Idle;
     }
-    
-    void Update()
+
+    private void Update()
     {
         MovementHandle();
     }
-    
+
     private IEnumerator Dash()
     {
         float _dashTimeCount = 0;
@@ -49,62 +56,73 @@ public class Player : MonoBehaviour
             SetPlayerState(PlayerState.Dash);
             yield return null;
         }
+
         SetPlayerState(PlayerState.Idle);
 
         yield return new WaitForSeconds(dashDelay);
         _isDash = false;
     }
+
     #endregion
 
     #region Method
+
     private void MovementHandle()
     {
         WalkHandle();
         SprintHandle();
         DashHandle();
-        
+
         Vector2 playerVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * _currentSpeed;
         _playerRigidbody2D.velocity = playerVelocity;
-        
-        if(playerVelocity == Vector2.zero) 
-            SetPlayerState(PlayerState.Idle);
+
+        _animator.SetTrigger(playerState.ToString());
+
+        // flip player horizontal direction
+        if (Input.GetAxisRaw("Horizontal") != 0)
+            transform.right = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
     }
-    
+
     private void WalkHandle()
     {
-        if(CheckPlayerState(PlayerState.Dash)) return;
-        
+        if (CheckPlayerState(PlayerState.Dash)) return;
+
         _currentSpeed = walkSpeed;
-        SetPlayerState(PlayerState.Walk);
+
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+            SetPlayerState(PlayerState.Idle);
+        else
+            SetPlayerState(PlayerState.Walk);
     }
-    
+
     private void SprintHandle()
     {
         if (CheckPlayerState(PlayerState.Dash)) return;
         if (CheckPlayerState(PlayerState.Idle)) return;
         if (!Input.GetKey(KeyCode.LeftShift)) return;
-        
+
         _currentSpeed = sprintSpeed;
         SetPlayerState(PlayerState.Sprint);
     }
-    
+
     private void DashHandle()
     {
         if (_isDash) return;
         if (CheckPlayerState(PlayerState.Idle)) return;
         if (!Input.GetKeyDown(KeyCode.LeftControl)) return;
-        
+
         StartCoroutine(Dash());
     }
-    
+
     private void SetPlayerState(PlayerState state)
     {
-        _playerState = state;
+        playerState = state;
     }
 
     private bool CheckPlayerState(PlayerState state)
     {
-        return _playerState == state;
+        return playerState == state;
     }
+
     #endregion
 }
