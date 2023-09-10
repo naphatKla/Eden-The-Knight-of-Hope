41a390461048ea14f32f3d80722dabff8175d7a3
Kla <naphat.watt@bumail.net>
2023-09-10 20:31:44 +0700

    Delete conflict files

--- Assets/Script/PlayerCombat.cs
+++ Assets/Script/PlayerCombat.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public Animator animator;
-
+    private Animator _animator;
     public LayerMask enemyLayers;
     public Transform attackPoint;
-    
     public int attackDamage = 40;
     public float attackRange = 0.5f;
-
     public float attackRate = 2f;
     private float nextAttackTime = 0f;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         if (Time.time < nextAttackTime || !Input.GetKeyDown(KeyCode.Mouse0)) return;
@@ -27,11 +28,11 @@ public class PlayerCombat : MonoBehaviour
 
     void Attack()
     {
-        animator.SetTrigger("Attack");
+        _animator.SetTrigger("Attack");
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,enemyLayers);
         
-        foreach (Collider2D enemy in hitEnemies)
-            enemy.GetComponent<Enemy01>().TakeDamage(attackDamage);
+        /*foreach (Collider2D enemy in hitEnemies)
+            enemy.GetComponent<Enemy01>().TakeDamage(attackDamage);*/
     }
 
     private void OnDrawGizmosSelected()
