347165452c2438f668d023a2332352cf77a4cc9c
Tartoruz <neoleodilx123@gmail.com>
2023-09-20 01:31:32 +0700

    Melee Combo

--- Assets/Script/PlayerCombatSystem.cs
+++ Assets/Script/PlayerCombatSystem.cs
@@ -12,9 +12,10 @@ public class PlayerCombatSystem : MonoBehaviour
     public Transform attackPoint;
     public int attackDamage = 40;
     public float attackRange = 0.5f;
-    public float attackRate = 2f;
+    public float attackRate = 1f;
     private float _nextAttackTime = 0f;
-    private int noOfClick = 0;
+    private int noOfClick = 0; 
+    private float currentCooldown = 0f;
     private GameObject _attackPointParent;
 
     private void Start()
@@ -26,13 +27,25 @@ public class PlayerCombatSystem : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(noOfClick);
+        Debug.Log(currentCooldown);
+        currentCooldown = currentCooldown - Time.deltaTime;
+        if (currentCooldown <= 0 )
+        {
+            currentCooldown = 0;
+            noOfClick = 0;
+        }
         if (Input.GetAxisRaw("Horizontal")  != 0)
             _attackPointParent.transform.right = new Vector3(Input.GetAxisRaw("Horizontal") , 0, 0);
         
-        if (Time.time < _nextAttackTime || !Input.GetKeyDown(KeyCode.Mouse0)) return;
         
+        if (Time.time < _nextAttackTime || !Input.GetKeyDown(KeyCode.Mouse0)) return;
         Attack();
+        noOfClick++;
+        currentCooldown = currentCooldown + attackRate+ 0.2f;
+        if (currentCooldown > attackRate+0.5f)
+        {
+            currentCooldown = attackRate + 0.5f;
+        }
         _nextAttackTime = Time.time + (1f / attackRate);
     }
 
@@ -52,7 +65,6 @@ public class PlayerCombatSystem : MonoBehaviour
             _animator.SetTrigger("Attack3");
             noOfClick = -1;
         }
-        noOfClick++;
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,enemyLayer);
         
         foreach (Collider2D enemy in hitEnemies) 
