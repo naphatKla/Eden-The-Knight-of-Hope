aa3873d3ea32742d3f89162f14ae5aee52fb0c73
ShaoKuy <iqtanawad@gmail.com>
2023-10-10 02:19:09 +0700

    Inventory102 Swap Item logic

--- Assets/Scripts/InventoryUI/UIInventoryItem.cs
+++ Assets/Scripts/InventoryUI/UIInventoryItem.cs
@@ -1,17 +1,86 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class UIInventoryItem : MonoBehaviour
+public class UIInventoryItem : MonoBehaviour, IPointerClickHandler, 
+    IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image itemImage;
+    [SerializeField] private TMP_Text quantityTxt;
+    [SerializeField] private Image borderImage;
+
+    public event Action<UIInventoryItem> OnItemClicked, OnItemDroppedOn, OnItemBeginDrag,
+        OnItemEndDrag, OnRightMouseBtnClick;
+    
+    private bool empty = true;
+
+    public void Awake()
     {
-        
+        ResetData();
+        Deselect();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetData()
+    {
+        this.itemImage.gameObject.SetActive(false);
+        empty = true;
+    }
+    
+    public void Deselect()
+    {
+        borderImage.enabled = false;
+    }
+
+    public void SetData(Sprite sprite, int quantity)
+    {
+        this.itemImage.gameObject.SetActive(true);
+        this.itemImage.sprite = sprite;
+        this.quantityTxt.text = quantity + "";
+        empty = false;
+    }
+
+    public void Select()
+    {
+        borderImage.enabled = true;
+    }
+    
+    
+    public void OnPointerClick(PointerEventData pointerData)
+    {
+        if (empty)
+            return;
+        if (pointerData.button == PointerEventData.InputButton.Right)
+        {
+            OnRightMouseBtnClick?.Invoke(this);
+        }
+        else
+        {
+            OnItemClicked.Invoke(this);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (empty)
+            return;
+        OnItemBeginDrag?.Invoke(this);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        OnItemEndDrag?.Invoke(this);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        OnItemDroppedOn?.Invoke(this);
+    }
+
+    public void OnDrag(PointerEventData eventData)
     {
         
     }
--- Assets/Scripts/InventoryUI/UIInventoryPage.cs
+++ Assets/Scripts/InventoryUI/UIInventoryPage.cs
@@ -2,13 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+using UnityEngine.UI;
 
 public class UIInventoryPage : MonoBehaviour
 {
     [SerializeField] private UIInventoryItem itemprefab;
     [SerializeField] private RectTransform contentPanel;
+    [SerializeField] private UIInventoryDescription itemDescription;
+    [SerializeField] private MouseFollower mouseFollower;
 
     private List<UIInventoryItem> listOfUIItems = new List<UIInventoryItem>();
+
+    public Sprite image, image2;
+    public int quantity;
+    public string title,description;
+    
+    private int currentlyDraggedItemIndex = -1;
+
+    public void Awake()
+    {
+        Hide();
+        mouseFollower.Toggle(false);
+        itemDescription.ResetDescription();
+    }
 
     public void InitializeInventoryUI(int inventorysize)
     {
@@ -17,12 +34,66 @@ public class UIInventoryPage : MonoBehaviour
             UIInventoryItem uiItem = Instantiate(itemprefab, Vector3.zero, Quaternion.identity);
             uiItem.transform.SetParent(contentPanel);
             listOfUIItems.Add(uiItem);
+
+            uiItem.OnItemClicked += HandleItemSelection;
+            uiItem.OnItemBeginDrag += HandleBeginDrag;
+            uiItem.OnItemDroppedOn += HandleSwap;
+            uiItem.OnItemEndDrag += HandleEndDrag;
+            uiItem.OnRightMouseBtnClick += HandleShowItemActions;
+
         }
+    }
+
+    private void HandleShowItemActions(UIInventoryItem inventoryItemUI)
+    {
+       
+    }
+
+    private void HandleEndDrag(UIInventoryItem inventoryItemUI)
+    {
+        mouseFollower.Toggle(false);
+    }
+
+    private void HandleSwap(UIInventoryItem inventoryItemUI)
+    {
+        int index = listOfUIItems.IndexOf(inventoryItemUI);
+        if (index == -1)
+        {
+            mouseFollower.Toggle(false);
+            currentlyDraggedItemIndex = -1;
+            return;
+        }
+        listOfUIItems[currentlyDraggedItemIndex].SetData(index == 0 ? image : image2, quantity);
+        listOfUIItems[index].SetData(currentlyDraggedItemIndex == 0 ? image : image2, quantity);
+        mouseFollower.Toggle(false);
+        currentlyDraggedItemIndex = -1;
+    }
+
+    private void HandleBeginDrag(UIInventoryItem inventoryItemUI)
+    {
+        int index = listOfUIItems.IndexOf(inventoryItemUI);
+        if (index == -1)
+            return;
+        currentlyDraggedItemIndex = index;
+        
+        
+        mouseFollower.Toggle(true);
+        mouseFollower.SetData(index == 0 ? image :image2 , quantity);
+    }
+
+    private void HandleItemSelection(UIInventoryItem inventoryItemUI)
+    {
+        itemDescription.SetDescription(image, title, description);
+        listOfUIItems[0].Select();
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
+        itemDescription.ResetDescription();
+        
+        listOfUIItems[0].SetData(image,quantity);
+        listOfUIItems[1].SetData(image2,quantity);
     }
 
     public void Hide()
